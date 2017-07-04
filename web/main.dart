8dac3d178d658dacb9f7eb1be2e1903675c1b59d
Adam Wonak <adam.wonak@gmail.com>
2017-07-03 21:25:03 -0500

    Added basic pwa code snippets.

--- web/main.dart
+++ web/main.dart
@@ -2,9 +2,13 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/platform/browser.dart';
-
 import 'package:angular_app/app_component.dart';
+import 'package:pwa/client.dart' as pwa;
+
 
 void main() {
+  // register PWA ServiceWorker for offline caching.
+  new pwa.Client();
+  
   bootstrap(AppComponent);
 }
