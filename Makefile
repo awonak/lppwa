0666c2f57ff0a4467f5574b7f666766945e57bf6
Adam Wonak <adam.wonak@gmail.com>
2017-07-28 23:49:21 -0500

    added static routes for build server. Added Makefile for handling build tasks.

---
+++ Makefile
@@ -0,0 +1,17 @@
+dartbuild:
+	@echo "Building AngularDart..."
+	pub build
+	@echo "Cleaning up temporary build files..."	
+	rm `find ./build/web -name *.ng_*.json`
+
+
+gobuild:
+	@echo "Building Go..."
+	cd ./server && go build -o ../build/web/server    
+
+build: dartbuild gobuild
+	@echo "Done!"
+
+run:
+	$(shell source ./server/.env)
+	cd ./build/web/ && ./server
--- server/main.go
+++ server/main.go
@@ -10,10 +10,24 @@ func main() {
 	r.Use(EnvConfig())
 	r.Use(CorsMiddleware())
 
-	v1 := r.Group("/api/v1")
+	// Static routes for serving front end
+	static := r.Group("")
 	{
-		v1.GET("/inventory/search", SearchInventory)
-		v1.GET("/inventory/category/:category", CategoryInventory)
+		static.StaticFile("/", "index.html")
+		static.StaticFile("/main.dart.js", "main.dart.js")
+		static.StaticFile("/pwa.dart.js", "pwa.dart.js")
+		static.StaticFile("/manifest.json", "manifest.json")
+		static.StaticFile("/favicon.png", "favicon.png")
+		static.Static("/css", "css")
+		static.Static("/img", "img")
+		static.Static("/packages", "packages")
+	}
+
+	// Routes for backend api server
+	apiV1 := r.Group("/api/v1")
+	{
+		apiV1.GET("/inventory/search", SearchInventory)
+		apiV1.GET("/inventory/category/:category", CategoryInventory)
 	}
 	r.Run(":9000") // listen and serve on 0.0.0.0:9000
 }
